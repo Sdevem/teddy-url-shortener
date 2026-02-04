b1b3a64520c805c8a7f5bb42d8fffe2d943eec9a
Sdevem Ramos <rs.sdevem@gmail.com>
2026-02-04 20:14:38 -0300

    feat(users): persist user with database migration

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   async create(data: CreateUserDto) {
-    return {
-      id: 'uuid-placeholder',
-      name: data.name,
-      email: data.email,
-    };
+    const user = await this.prisma.user.create({
+      data,
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return user;
   }
 }
